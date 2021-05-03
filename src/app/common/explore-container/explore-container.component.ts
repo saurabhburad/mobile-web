4d0cca487e084154b08814606b38587baa9dc52b
saurabh burad <saurabhburad@gmail.com>
2021-05-03 23:02:41 +0530

    Added camera

--- src/app/common/explore-container/explore-container.component.ts
+++ src/app/common/explore-container/explore-container.component.ts
@@ -13,7 +13,7 @@ export class ExploreContainerComponent implements OnInit {
   @Output() image = new EventEmitter<string>();
   options: CameraOptions = {
     quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
+    destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
   }
