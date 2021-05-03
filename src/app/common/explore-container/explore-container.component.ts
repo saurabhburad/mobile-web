b7e1b33ad66b62f0747edd536233ce0098a57ad9
saurabh burad <saurabhburad@gmail.com>
2021-05-03 23:34:24 +0530

    Added camera

--- src/app/common/explore-container/explore-container.component.ts
+++ src/app/common/explore-container/explore-container.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { Platform } from '@ionic/angular';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
   styleUrls: ['./explore-container.component.scss'],
-  providers: [Camera]
+  providers: [Camera,NativeStorage]
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
   @Output() image = new EventEmitter<string>();
   options: CameraOptions = {
-    quality: 100,
+    quality: 10,
     destinationType: this.camera.DestinationType.DATA_URL,
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
   }
 
-  constructor(public camera: Camera,public platform:Platform) { }
+  constructor(public camera: Camera,public platform:Platform,private nativeStorage: NativeStorage) { }
 
 
   ngOnInit() {}
@@ -39,8 +40,16 @@ export class ExploreContainerComponent implements OnInit {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64 (DATA_URL):
       let base64OrImage = 'data:image/jpeg;base64,' + imageData;
-              localStorage.setItem('base64OrImage', JSON.stringify(base64OrImage));
-              this.image.emit(base64OrImage);
+      this.image.emit(base64OrImage);
+
+      this.nativeStorage.setItem('myitem', { property: JSON.stringify(base64OrImage) })
+        .then(
+          () => {
+            this.image.emit(base64OrImage);
+            console.log('Stored item!')
+          },
+          error => console.error('Error storing item', error)
+        );
      }, (err) => {
       // Handle error
      });
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  providers: [NativeStorage]
+
 })
 export class Tab2Page {
 
   base64OrImage="https://images.pexels.com/photos/907267/pexels-photo-907267.jpeg?auto=compress&cs=tinysrgb&h=350";
 
-  constructor() { 
-    let oldData = (JSON.parse(localStorage.getItem('base64OrImage')));
-    this.base64OrImage = oldData ? oldData:this.base64OrImage
+  constructor(private nativeStorage: NativeStorage) {
+    this.nativeStorage.getItem('myitem')
+      .then(
+        data => {
+          this.base64OrImage = data ? data : this.base64OrImage
+          console.log(data)
+        },
+        error => console.error(error)
+      );
   }
- 
+
   
   addItem(image) {
     this.base64OrImage = image;
